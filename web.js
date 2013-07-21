6ed7715f21fbd4f84760d97945a48f9b3c6f7615
Ubuntu <ubuntu@ip-172-31-37-114.us-west-2.compute.internal>
2013-07-21 13:08:39 +0000

    commit 1

--- web.js
+++ web.js
@@ -1,12 +1,15 @@
 var express = require('express');
-var app = express();
-app.use(express.logger());
+var fs = require('fs');
+
+var app = express.createServer(express.logger());
+
+var fname = "index.html";
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(fs.readFileSync(fname, "utf-8"));
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
